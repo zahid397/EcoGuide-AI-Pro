e18ccea8af41539a9841ccfb586e03c7ea314b78
zahid397 <zh05698@gmail.com>
2025-11-17 20:11:03 +0600

    Update caching.py

--- utils/caching.py
+++ utils/caching.py
@@ -1,11 +1,11 @@
 from functools import lru_cache
-from backend.agent_workflow import TravelAgent
+from backend.agent_workflow import AgentWorkflow
 from backend.rag_engine import RAGEngine
 
 @lru_cache(maxsize=1)
 def get_agent():
-    """Return a cached instance of the TravelAgent."""
-    return TravelAgent()
+    """Return a cached instance of the AgentWorkflow."""
+    return AgentWorkflow()
 
 @lru_cache(maxsize=1)
 def get_rag():
