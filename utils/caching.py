a4689b8b74220af02bfda1743cfebf5248891997
zahid397 <zh05698@gmail.com>
2025-11-18 08:01:50 +0600

    Update caching.py

--- utils/caching.py
+++ utils/caching.py
@@ -1,11 +1,11 @@
 from functools import lru_cache
-from backend.rag_engine import RAGEngine
 from backend.agent_workflow import AgentWorkflow
-
-@lru_cache(maxsize=1)
-def get_rag():
-    return RAGEngine()
+from backend.rag_engine import RAGEngine  # This should still be used for search
 
 @lru_cache(maxsize=1)
 def get_agent():
     return AgentWorkflow()
+
+@lru_cache(maxsize=1)
+def get_rag():
+    return RAGEngine()
