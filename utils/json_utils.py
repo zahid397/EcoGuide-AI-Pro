cbb24493d8631172fd43870c624755239ecc1688
zahid397 <zh05698@gmail.com>
2025-11-18 18:12:52 +0600

    Delete utils/json_utils.py

--- utils/json_utils.py
+++
@@ -1,9 +0,0 @@
-import json
-import re
-
-def extract_json(text: str):
-    try:
-        match = re.search(r"\{.*\}", text, re.DOTALL)
-        return json.loads(match.group(0))
-    except:
-        return {}
