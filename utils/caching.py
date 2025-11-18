b8cacc996dc73395a7aa9c7cb5e7d46a74f1fd3d
zahid397 <zh05698@gmail.com>
2025-11-18 07:06:07 +0600

    Update caching.py

--- utils/caching.py
+++ utils/caching.py
@@ -1,11 +1,11 @@
 from functools import lru_cache
+from backend.rag_engine import RAGEngine
 from backend.agent_workflow import AgentWorkflow
-from backend.rag_engine import HybridRAG   # <-- use NEW engine
+
+@lru_cache(maxsize=1)
+def get_rag():
+    return RAGEngine()
 
 @lru_cache(maxsize=1)
 def get_agent():
     return AgentWorkflow()
-
-@lru_cache(maxsize=1)
-def get_rag():
-    return HybridRAG()    # <-- FIXED
